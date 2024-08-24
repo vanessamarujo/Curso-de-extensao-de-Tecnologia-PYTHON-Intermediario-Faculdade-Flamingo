143f06acd6804435ea8ff47e889185dd32db792d
Vanessa Marujo <vanessa_marujo@yahoo.com.br>
2024-08-24 11:06:41 -0300

    Add files via upload

---
+++ exemplo05.py
@@ -0,0 +1,16 @@
+# Curso Intermediário de PYTHON
+# Nome da desenvolvedora: Vanessa Marujo Nogueira
+# Versão 1.0
+# Exercício de lógica de programação com a linguagem de programação PYTHON
+# Exercício de Lista
+
+#Uma lista simples
+list1= [1,2,"Python","Programa",15.9]
+list2= ["Amy","Ryan","Henry","Emma"]
+
+#imprimindo a lista
+print(list1)
+print(list2)
+#imprimindo o tipo de lista
+print(type(list1))
+print(type(list2))
