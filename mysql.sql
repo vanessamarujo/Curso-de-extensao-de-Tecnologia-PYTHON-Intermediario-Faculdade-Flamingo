40218eda0297d25905d66b85b5ebee2d919a5373
Vanessa Marujo <vanessa_marujo@yahoo.com.br>
2024-10-05 15:00:23 -0300

    Add files via upload

---
+++ mysql.sql
@@ -0,0 +1,36 @@
+create database flamingo;
+create database flamingo2;
+create database flamingo3;
+create database flamingo4;
+create database flamingo5;
+DROP DATABASE flamingo;
+DROP DATABASE flamingo2;
+DROP DATABASE flamingo3;
+DROP DATABASE flamingo4;
+DROP DATABASE flamingo5;
+create database flamingo;
+create table aluno (
+PersonID int,
+LastName varchar(255),
+FirstName varchar(255),
+Address varchar(255),
+City varchar(255)
+);
+
+use flamingo;
+DROP DATABASE FAMINGO;
+SELECT * FROM ALUNO;
+INSERT INTO ALUNO (PersonID, LastName, FirstName, Address, City) VALUES (1,"MARUJO","VANESSA","RUA BRUNO CHATO DA SILVA", "LAPA");
+INSERT INTO ALUNO (PersonID, LastName, FirstName, Address, City) VALUES (2,"SILVA","PIETRO","RUA UM DOIS TRES","CAIEIRAS");
+INSERT INTO ALUNO (PersonID, LastName, FirstName, Address, City) VALUES (3,"GONCALVES","LORENZO","RUA DOIS SETE","FRANCO DA ROCHA");
+INSERT INTO ALUNO (PersonID, LastName, FirstName, Address, City) VALUES (4, "VIEIRA","BRUNO","RUA DOIS DOIS UM","PERUS");
+INSERT INTO ALUNO (PersonID, LastName, FirstName, Address, City) VALUES (5, "PEREIRA","LAURA", "RUA CANARIO","PIRITUBA");
+
+create table professor(
+
+);
+
+create table 
+	
+
+
